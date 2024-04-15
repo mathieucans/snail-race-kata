7b511cbfb5f214ff9520e067999942efc4307818
mathieu cans <mathieu.cans@orange.com>
2024-04-15 10:01:54 +0200

    Display race results

--- snail-race-server/src/index.ts
+++ snail-race-server/src/index.ts
@@ -3,8 +3,10 @@ import express, {Request, Response} from "express";
 const app = express()
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Express & TypeScript Server');
+app.get('/results', (req: Request, res: Response) => {
+    res.json({races:[
+            {id:123465, date:Date.now(), podium:[{number:5, name : 'Wind'}, {number:3, name:'Jacky brown'}, {number:22, name:'bill'}]}
+        ]})
 });
 
 app.listen(port, () => {
