4fa0e9464c9be8e369980e45507e3018036c90f3
Johan Martinsson <martinsson@users.noreply.github.com>
2024-05-03 16:19:41 +0200

    one solution for contract testing with setup;

--- snail-race-bet/typescript/src/adapters/MongoDbBetRepository.spec.ts
+++ snail-race-bet/typescript/src/adapters/MongoDbBetRepository.spec.ts
@@ -1,53 +1,76 @@
 import {MongoClient} from "mongodb";
 import {MongoDbBetRepository} from "./MongoDbBetRepository";
 import {Bet, PodiumPronostic} from "../domain/Bet";
+import {BetRepository} from "../domain/BetRepository";
+
+class ContractTest {
+    private repository!: BetRepository;
+    private mongoClient!: MongoClient;
+
+    withRepo(repository: BetRepository, mongoClient: MongoClient) {
+        this.mongoClient = mongoClient;
+        this.repository = repository;
+    }
+
+    registerTests() {
+
+        test('get a bet within the time range', async () => {
+            let middleOfDay = new Date(2020, 1, 1, 12, 0, 0).getTime();
+            let registeredBet = new Bet("trump", new PodiumPronostic(1, 2, 3),middleOfDay);
+            await this.repository.register(registeredBet)
+
+            let startOfDay = new Date(2020, 1, 1, 0, 0, 0).getTime();
+            let endOfDay = new Date(2020, 1, 1, 23, 59, 59).getTime();
+            const bets = await this.repository.findByDateRange(startOfDay, endOfDay)
+
+            expect(bets).toHaveLength(1)
+            expect(bets).toContainEqual(registeredBet)
+        })
+
+        // TODO rename with do not retrive bets outside date range
+        test('retrieve bet by time range', async () => {
+            let nextDay = new Date(2020, 1, 2, 0, 0, 0).getTime();
+            let registeredBet = new Bet("trump", new PodiumPronostic(1, 2, 3),nextDay);
+            await this.repository.register(registeredBet)
+
+            let startOfDay = new Date(2020, 1, 1, 0, 0, 0).getTime();
+            let endOfDay = new Date(2020, 1, 1, 23, 59, 59).getTime();
+            const bets = await this.repository.findByDateRange(startOfDay, endOfDay)
+
+            expect(bets).toHaveLength(0)
+
+
+        })
+    }
+
+    after() {
+        this.mongoClient.close()
+    }
+}
 
 describe('MongoDbBetRepository', () => {
-    let mongoClient: MongoClient;
-    let repository: MongoDbBetRepository;
+
+    let  contractTest = new ContractTest(
+
+    )
 
     beforeEach(async () => {
-        mongoClient = new MongoClient('mongodb://localhost:27017');
+        const mongoClient = new MongoClient('mongodb://localhost:27017');
         await mongoClient.connect()
         const db = mongoClient.db('contract_testing');
         await db.collection("bets").drop();
 
-        repository = new MongoDbBetRepository(db);
+        const repository = new MongoDbBetRepository(db);
+
+        contractTest.withRepo(repository, mongoClient)
     })
 
     afterEach(() => {
-        mongoClient.close()
-    })
-
-
-    // TODO
-    // change
-
-    test('get a bet within the time range', async () => {
-        let middleOfDay = new Date(2020, 1, 1, 12, 0, 0).getTime();
-        let registeredBet = new Bet("trump", new PodiumPronostic(1, 2, 3),middleOfDay);
-        await repository.register(registeredBet)
-
-        let startOfDay = new Date(2020, 1, 1, 0, 0, 0).getTime();
-        let endOfDay = new Date(2020, 1, 1, 23, 59, 59).getTime();
-        const bets = await repository.findByDateRange(startOfDay, endOfDay)
-
-        expect(bets).toHaveLength(1)
-        expect(bets).toContainEqual(registeredBet)
-    })
-
-    // TODO rename with do not retrive bets outside date range
-    test('retrieve bet by time range', async () => {
-        let nextDay = new Date(2020, 1, 2, 0, 0, 0).getTime();
-        let registeredBet = new Bet("trump", new PodiumPronostic(1, 2, 3),nextDay);
-        await repository.register(registeredBet)
-
-        let startOfDay = new Date(2020, 1, 1, 0, 0, 0).getTime();
-        let endOfDay = new Date(2020, 1, 1, 23, 59, 59).getTime();
-        const bets = await repository.findByDateRange(startOfDay, endOfDay)
-
-        expect(bets).toHaveLength(0)
-
+        contractTest.after()
 
     })
+
+    contractTest.registerTests()
+
+
 })
