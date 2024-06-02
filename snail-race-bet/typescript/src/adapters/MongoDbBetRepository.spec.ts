8cf9d268d3c60f7a5ced6fb1e320974632bc110e
Johan Martinsson <martinsson@users.noreply.github.com>
2024-06-03 00:43:52 +0200

    contract test for the provider

--- snail-race-bet/typescript/src/adapters/MongoDbBetRepository.spec.ts
+++ snail-race-bet/typescript/src/adapters/MongoDbBetRepository.spec.ts
@@ -1,5 +1,6 @@
 import {MongoClient} from "mongodb";
 import {MongoDbBetRepository} from "./MongoDbBetRepository";
+import {Bet, PodiumPronostic} from "../domain/BetRepository";
 
 describe('MongoDbBetRepository', () => {
     let mongoClient: MongoClient;
@@ -19,10 +20,14 @@ describe('MongoDbBetRepository', () => {
     })
 
     test('register a bets', async () => {
-        expect(true).toEqual(false);
+        await repository.register(new Bet("me", new PodiumPronostic(2, 4, 6), Date.parse("2021-01-01T00:00:00Z")));
+        const result = await repository.findByDateRange(Date.parse("2021-01-01T00:00:00Z"), Date.parse("2021-01-02T00:00:00Z"));
+        expect(result).toEqual([new Bet("me", new PodiumPronostic(2, 4, 6), Date.parse("2021-01-01T00:00:00Z"))]);
     })
 
     test('retrieve only bets inside the time range', async () => {
-        expect(true).toEqual(false);
+        await repository.register(new Bet("me", new PodiumPronostic(2, 4, 6), Date.parse("2021-01-01T00:00:00Z")));
+        const result = await repository.findByDateRange(Date.parse("2021-01-01T00:00:01Z"), Date.parse("2021-01-02T00:00:00Z"));
+        expect(result).toEqual([]);
     })
 })
