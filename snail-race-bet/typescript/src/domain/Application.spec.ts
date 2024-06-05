260e05d9c732889ca7959f202e512613b250cba3
mathieu cans <mathieu.cans@orange.com>
2024-06-05 17:24:08 +0200

    Improve test readability by using TestableApplication instead of before each

--- snail-race-bet/typescript/src/domain/Application.spec.ts
+++ snail-race-bet/typescript/src/domain/Application.spec.ts
@@ -1,81 +1,74 @@
-import {FakeRaceResultProvider} from "./FakeRaceResultProvider";
-import {FakeBetRepository} from "./FakeBetRepository";
 import {Podium, Snail} from "./RaceResultProvider";
 import {noWinner, Winners} from "./Winners";
-import {Application} from "./Application";
+import {TestableApplication} from "./TestableApplication";
 
 
 describe('Gambler', () => {
 
     it('should not win when no Bet is placed', async () => {
-        const app = new Application(new FakeBetRepository(), new FakeRaceResultProvider())
+        const app = new TestableApplication()
 
         const result = await app.getWinners()
         expect(result).toEqual(noWinner)
     });
 
     it('should win when the podium exactly matches the bet', async () => {
-        const raceResultProvider = new FakeRaceResultProvider();
-        const app = new Application(new FakeBetRepository(), raceResultProvider)
+        const app = new TestableApplication();
 
         let betTime = Date.parse("2021-01-01T00:00:00Z");
         await app.placeBet("me",betTime, 1, 2, 3);
 
         let fourMinutesLater = Date.parse("2021-01-01T00:04:00Z");
         let podium = new Podium(new Snail(1, 'Turbo'), new Snail(2, 'Flash'), new Snail(3, 'Speedy'));
-        raceResultProvider.simulateRaceResult(fourMinutesLater, podium)
+        app.simulateRaceResult(fourMinutesLater, podium)
 
         const result = await app.getWinners()
         expect(result).toEqual(new Winners(["me"]))
     });
 
     it(`should not win when the podium don't match exactly the bet`, async () => {
-        const raceResultProvider = new FakeRaceResultProvider();
-        const app = new Application(new FakeBetRepository(), raceResultProvider)
+        const app = new TestableApplication();
 
         let betTime = Date.parse("2021-01-01T00:00:00Z");
         await app.placeBet("me",betTime, 1, 2, 4);
 
         let fourMinutesLater = Date.parse("2021-01-01T00:04:00Z");
         let podium = new Podium(new Snail(1, 'Turbo'), new Snail(2, 'Flash'), new Snail(3, 'Speedy'));
-        raceResultProvider.simulateRaceResult(fourMinutesLater, podium)
+        app.simulateRaceResult(fourMinutesLater, podium)
 
         const result = await app.getWinners()
         expect(result).toEqual(noWinner)
     })
 
     it(`should not win when the bet has been placed less than 3 seconds before the race date`, async () => {
-        const raceResultProvider = new FakeRaceResultProvider();
-        const app = new Application(new FakeBetRepository(), raceResultProvider)
+        const app = new TestableApplication();
 
         let podium = new Podium(new Snail(1, 'Turbo'), new Snail(2, 'Flash'), new Snail(3, 'Speedy'));
         let betTime = Date.parse("2021-01-01T00:00:00Z");
 
         await app.placeBet("me",betTime, 1, 2, 3);
         let twoSecondsLater = Date.parse("2021-01-01T00:00:02Z");
-        raceResultProvider.simulateRaceResult(twoSecondsLater, podium)
+        app.simulateRaceResult(twoSecondsLater, podium)
 
         const result = await app.getWinners()
         expect(result).toEqual(noWinner)
     });
 
     it(`should not win when the bet is older than the previous race`, async () => {
-        const raceResultProvider = new FakeRaceResultProvider();
-        const app = new Application(new FakeBetRepository(), raceResultProvider)
+        const app = new TestableApplication();
 
         let betTime = Date.parse("2021-01-01T00:00:00Z");
         await app.placeBet("me",betTime, 1, 2, 3);
 
         let fourMinutesLater = Date.parse("2021-01-01T00:04:00Z");
         let nonMatchingPodium = new Podium(new Snail(5, 'Billy'), new Snail(7, 'Darkness'), new Snail(8, 'Speedup'));
-        raceResultProvider.simulateRaceResult(fourMinutesLater, nonMatchingPodium)
+        app.simulateRaceResult(fourMinutesLater, nonMatchingPodium)
 
         let nineMinutesLater = Date.parse("2021-01-01T00:09:00Z");
         let matchingPodium = new Podium(new Snail(1, 'Turbo'), new Snail(2, 'Flash'), new Snail(3, 'Speedy'));
-        raceResultProvider.simulateRaceResult(nineMinutesLater, matchingPodium)
+        app.simulateRaceResult(nineMinutesLater, matchingPodium)
 
         const result = await app.getWinners()
         expect(result).toEqual(noWinner)
-
     });
 });
---
+++ snail-race-bet/typescript/src/domain/TestableApplication.ts
@@ -0,0 +1,26 @@
+import {Application} from "./Application";
+import {FakeRaceResultProvider} from "./FakeRaceResultProvider";
+import {FakeBetRepository} from "./FakeBetRepository";
+import {Podium} from "./RaceResultProvider";
+
+export class TestableApplication {
+    private readonly app: Application;
+    private readonly fakeRaceResultProvider: FakeRaceResultProvider;
+
+    constructor() {
+        this.fakeRaceResultProvider = new FakeRaceResultProvider();
+        this.app = new Application(new FakeBetRepository(), this.fakeRaceResultProvider);
+    }
+
+    async getWinners() {
+        return this.app.getWinners()
+    }
+
+    async placeBet(gambler: string, timestamp: number, first: number, second: number, third: number) {
+        return this.app.placeBet(gambler, timestamp, first, second, third)
+    }
+
+    simulateRaceResult(date: number, podium: Podium) {
+        this.fakeRaceResultProvider.simulateRaceResult(date, podium)
+    }
+}
