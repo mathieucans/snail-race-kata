d7e80da1a2462af90b5cbf673c90688e15f483ef
mathieu cans <mathieu.cans@orange.com>
2024-06-05 16:25:28 +0200

    Gambler should not win when the podium don't match exactly the bet

--- snail-race-bet/typescript/src/domain/Application.spec.ts
+++ snail-race-bet/typescript/src/domain/Application.spec.ts
@@ -6,6 +6,7 @@ import {Application} from "./Application";
 
 
 describe('Gambler', () => {
+
     it('should not win when no Bet is placed', async () => {
         const app = new Application(new FakeBetRepository(), new FakeRaceResultProvider())
         const result = await app.getWinners(Date.parse("2021-01-01T00:00:00Z"))
@@ -25,6 +26,21 @@ describe('Gambler', () => {
         const result = await app.getWinners(fourMinutesLater)
 
         expect(result).toEqual(new Winners(["me"]))
+    });
+
+    it(`should not win when the podium don't match exactly the bet`, async () => {
+        const raceResultProvider = new FakeRaceResultProvider();
+        const app = new Application(new FakeBetRepository(), raceResultProvider)
+
+        let podium = new Podium(new Snail(1, 'Turbo'), new Snail(2, 'Flash'), new Snail(3, 'Speedy'));
+        let betTime = Date.parse("2021-01-01T00:00:00Z");
+
+        await app.placeBet("me",betTime, 1, 2, 4);
+        let fourMinutesLater = Date.parse("2021-01-01T00:04:00Z");
+        raceResultProvider.simulateRaceResult(fourMinutesLater, podium)
+        const result = await app.getWinners(fourMinutesLater)
+
+        expect(result).toEqual(new Winners([]))
 
     });
 });
--- snail-race-bet/typescript/src/domain/GetWinnersUseCase.ts
+++ snail-race-bet/typescript/src/domain/GetWinnersUseCase.ts
@@ -1,6 +1,15 @@
-import {BetRepository} from "./BetRepository";
-import {RaceResultProvider} from "./RaceResultProvider";
+import {BetRepository, PodiumPronostic} from "./BetRepository";
+import {Podium, RaceResultProvider} from "./RaceResultProvider";
 import {Winners} from "./Winners";
+
+function matchExactly(pronostic: PodiumPronostic, podium: Podium) : boolean {
+    return pronostic.first === podium.first.number
+        && pronostic.second === podium.second.number
+        && pronostic.third === podium.third.number;
+}
+
+const noWinner = new Winners([]);
+
 
 export class GetWinnersUseCase {
     constructor(private betRepository: BetRepository, private raceResultProvider: RaceResultProvider) {
@@ -9,7 +18,15 @@ export class GetWinnersUseCase {
 
     async getWinners(date: number) {
         const bets = await this.betRepository.findByDateRange(0, date)
+        const results = await this.raceResultProvider.races();
 
-        return new Winners(bets.map(bet => bet.gambler))
+        if(results.races.length ===0 ){
+            return noWinner;
+        }
+        const lastRacePodium = results.races[0].podium;
+
+        return new Winners(bets
+            .filter(bet => matchExactly(bet.pronostic, lastRacePodium))
+            .map(bet => bet.gambler))
     }
 }
