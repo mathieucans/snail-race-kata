206289ac9edd198b0b7cc1bd33ff9d1ee11a37a3
Jérôme Avoustin <jerome.avoustin@sundayapp.com>
2024-06-07 11:43:18 +0200

    make test pass

--- snail-race-bet/java/src/main/java/snail/race/kata/adapters/MongoDbBetRepository.java
+++ snail-race-bet/java/src/main/java/snail/race/kata/adapters/MongoDbBetRepository.java
@@ -4,19 +4,29 @@ import com.mongodb.client.MongoDatabase;
 import snail.race.kata.domain.Bet;
 import snail.race.kata.domain.BetRepository;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.StreamSupport;
 
 public class MongoDbBetRepository implements BetRepository {
+    private final MongoDatabase database;
+
     public MongoDbBetRepository(MongoDatabase database) {
 
+        this.database = database;
     }
 
     @Override
     public void register(Bet bet) {
+        database.getCollection("bet", Bet.class).insertOne(bet);
     }
 
     @Override
     public List<Bet> findByDateRange(int from, int to) {
-        return null;
+        Spliterator<Bet> bet = database.getCollection("bet", Bet.class).find().spliterator();
+        return StreamSupport.stream(bet, false).filter(b -> b.timestamp() > from && b.timestamp() < to).toList();
     }
 }
