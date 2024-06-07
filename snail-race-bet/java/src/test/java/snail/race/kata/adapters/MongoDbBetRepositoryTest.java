4ce642a4cd6fa340484827c9bca4bc561e2e7b9e
mathieu cans <mathieu.cans@orange.com>
2024-06-07 06:41:29 +0200

    [java] Add MongoDbBetRepository skeleton and its test

---
+++ snail-race-bet/java/src/test/java/snail/race/kata/adapters/MongoDbBetRepositoryTest.java
@@ -0,0 +1,50 @@
+package snail.race.kata.adapters;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import snail.race.kata.domain.Bet;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+public class MongoDbBetRepositoryTest {
+    MongoClient mongoClient;
+    MongoDbBetRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+        mongoClient = MongoClients.create("mongodb://localhost");
+        MongoDatabase database = mongoClient.getDatabase("contract_testing").withCodecRegistry(pojoCodecRegistry);
+
+        database.getCollection("bet", Bet.class).drop();
+
+        repository = new MongoDbBetRepository(database);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        mongoClient.close();
+    }
+
+    @Test
+    void register_a_bets() {
+        assertThat(true).isFalse();
+    }
+
+    @Test
+    void retrieve_only_bets_inside_the_time_range() {
+        assertThat(true).isFalse();
+    }
+}
