2818fee606774a6da5bdc18ace4a8fb3e586c7bd
Johan Martinsson <martinsson@users.noreply.github.com>
2024-06-11 01:30:39 +0200

    faking it step 3

--- snail-race-bet/java/src/main/java/snail/race/kata/domain/BetApplication.java
+++ snail-race-bet/java/src/main/java/snail/race/kata/domain/BetApplication.java
@@ -1,8 +1,12 @@
 package snail.race.kata.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BetApplication {
+    private boolean betPlaced;
+
     BetApplication(
             BetRepository betRepository,
             RaceResultProvider raceResultProvider) {
@@ -14,9 +18,14 @@ public class BetApplication {
              int first,
              int second,
              int third) {
+
+        this.betPlaced = true;
     }
 
     List<Winner> getWinnersForLastRace() {
-        return null;
+        if (!betPlaced) {
+            return List.of();
+        }
+        return List.of(new Winner("me"));
     }
 }
--- snail-race-bet/java/src/test/java/snail/race/kata/domain/BetApplicationTest.java
+++ snail-race-bet/java/src/test/java/snail/race/kata/domain/BetApplicationTest.java
@@ -3,6 +3,7 @@ package snail.race.kata.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BetApplicationTest {
@@ -18,8 +19,17 @@ class BetApplicationTest {
     @Test
     void no_winners_when_no_bet_is_placed() {
         var winners = betApplication.getWinnersForLastRace();
-        assertTrue(winners.isEmpty());
+        assertThat(winners).isEmpty();
     }
 
-
+    @Test
+    void winners_when_there_is_an_exact_match() {
+        betApplication.placeBet("me", 1, 9, 8, 7);
+        raceResultProvider.simulateRaceResult(33, 1, new RaceResultProvider.Podium(
+                new RaceResultProvider.Snail(9, "Turbo"),
+                new RaceResultProvider.Snail(8, "Flash"),
+                new RaceResultProvider.Snail(7, "Speedy")
+        ));
+        assertThat(betApplication.getWinnersForLastRace()).containsExactly(new Winner("me"));
+    }
 }
