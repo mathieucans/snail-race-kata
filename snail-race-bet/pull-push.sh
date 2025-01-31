a9b7739a2ce13cc6816ee966ac9000ad9c683aea
mathieu cans <mathieu.cans@orange.com>
2025-01-31 18:02:23 +0100

    protect script against comiiting on main or solution

--- snail-race-bet/pull-push.sh
+++ snail-race-bet/pull-push.sh
@@ -5,12 +5,13 @@
 
 export CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 
-if $CURRENT_BRANCH == 'main'
-then
+if [ "$CURRENT_BRANCH" = "main" ] || [ "$CURRENT_BRANCH" = "start-step2" ] || [ "$CURRENT_BRANCH" = "start-step3" ] || [ "$CURRENT_BRANCH" = "solution" ]; then
     echo 'Create your own branch before use this script'
+    exit 1
 fi
 
 git pull origin "$CURRENT_BRANCH"
 git add .
 git commit -m 'Working...'
 git push origin "$CURRENT_BRANCH"
+
