a09bc8a6e316cc689898bb679c3c72731182f979
mathieu cans <mathieu.cans@orange.com>
2025-02-12 09:08:17 +0100

    Update watch script with waiting 20s between commit

--- snail-race-bet/watch.sh
+++ snail-race-bet/watch.sh
@@ -2,4 +2,12 @@
 
 # Loops on git pull-push in a remote mob
 
-while true; do  ./pull-push.sh; done
+while true;
+do
+echo "wait 20s"
+sleep 10
+echo "wait 10s"
+sleep 10
+echo "pull/push"
+./pull-push.sh;
+done
