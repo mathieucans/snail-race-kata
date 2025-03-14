250d302829c8c4605d816a69c3c1c8b51a4b9e1a
mathieu cans <mathieu.cans@orange.com>
2025-03-14 11:21:50 +0100

    Add application signature for start step 3

--- snail-race-bet/typescript/src/domain/BetApplication.ts
+++ snail-race-bet/typescript/src/domain/BetApplication.ts
@@ -1,5 +1,6 @@
 import {BetRepository} from "./BetRepository";
 import {RaceResultProvider} from "./RaceResultProvider";
+import {Winners} from "./Winners";
 
 export class BetApplication {
     constructor(
@@ -8,11 +9,11 @@ export class BetApplication {
 
     }
 
-    async placeBet(gambler: string, timestamp: number, first: number, second: number, third: number) {
-
+    async placeBet(gambler: string, timestamp: number, first: number, second: number, third: number) : Promise<void> {
+        throw 'Not yet implemented'
     }
 
-    async getWinnersForLastRace() {
-        return undefined;
+    async getWinnersForLastRace() : Promise<Winners> {
+        throw 'Not yet implemented'
     }
 }
