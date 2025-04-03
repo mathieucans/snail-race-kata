d03d39264c7946c0e5a487f18f1230b2aade4b79
mathieu cans <mathieu.cans@orange.com>
2025-04-03 16:04:27 +0200

    Remove use less jackson annotations

--- snail-race-bet/java/src/main/java/snail/race/kata/adapters/RaceResultProviderHttprRecords.java
+++ snail-race-bet/java/src/main/java/snail/race/kata/adapters/RaceResultProviderHttprRecords.java
@@ -5,28 +5,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class RaceResultProviderHttprRecords {
-    private final String baseUrl;
-
-    public RaceResultProviderHttprRecords(String baseUrl) {
-        this.baseUrl = baseUrl;
-    }
 
     public record Races(
-            @JsonProperty("races") List<Race> races
+            List<Race> races
     ) {
     }
 
     public record Race(
-            @JsonProperty("raceId") int raceId,
-            @JsonProperty("timestamp") long timestamp,
-            @JsonProperty("snails") List<Snail> snails
+            int raceId,
+            long timestamp,
+            List<Snail> snails
     ) {
     }
 
     public record Snail(
-            @JsonProperty("number") int number,
-            @JsonProperty("name") String name,
-            @JsonProperty("duration") double duration
+            int number,
+            String name,
+            double duration
     ) {
     }
 }
