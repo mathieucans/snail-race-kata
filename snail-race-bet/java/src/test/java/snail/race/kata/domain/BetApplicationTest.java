c3e67c3b7f844c02c2376dacdbc71d4adb345bbf
mathieu cans <mathieu.cans@orange.com>
2025-04-04 13:10:51 +0200

    Add test shell to help participant at step 3

--- snail-race-bet/java/src/test/java/snail/race/kata/domain/BetApplicationTest.java
+++ snail-race-bet/java/src/test/java/snail/race/kata/domain/BetApplicationTest.java
@@ -1,6 +1,8 @@
 package snail.race.kata.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,14 +14,52 @@ class BetApplicationTest {
 
     @BeforeEach
     void setUp() {
-        betApplication = new BetApplication(new BetRepositoryFake(), raceResultProvider);
+
+        betApplication = new BetApplication(
+                new BetRepositoryFake(), // You don't need to interact with the repository to test the entire domain !
+                raceResultProvider);
     }
 
     @Test
-    void no_winners_when_no_bet_is_placed() {
+    void no_winner_when_no_bet_is_placed() {
         var winners = betApplication.getWinnersForLastRace();
         assertTrue(winners.isEmpty());
     }
 
+    @Nested
+    class WinOnlyWhenPronosticExactMatchsPodium {
+        @Test
+        void exact_match() {
+            // Place a bet through betApplication
+            // Configure race result provider simulator to have the corresponding podium
+            // Verify winners
+            Assertions.fail("Write the test and implement it");
+        }
 
+        @Test
+        void third_place_differs() {
+            // Place a bet through betApplication
+            // Configure race result provider simulator to have another podium
+            // Verify there is no winner
+            Assertions.fail("Write the test and implement it");
+        }
+    }
+
+
+    @Test
+    void no_winner_when_bet_is_placed_less_than_3_seconds() {
+        // Place a bet through betApplication
+        // Configure race result provider simulator to have the corresponding podium but with a timestamp less than 3 seconds
+        // Verify there is no winner
+        Assertions.fail("Write tests and implement it");
+    }
+
+    @Test
+    void no_winner_when_the_bet_is_older_than_the_previous_race() {
+        // Place a bet through betApplication
+        // Configure a race that is older than the bet
+        // Configure another race that is newer than the previous race and has a podium that match the pronostic
+        // Verify there is no winner
+        Assertions.fail("Write tests and implement it");
+    }
 }
--- snail-race-bet/typescript/src/domain/BetApplication.spec.ts
+++ snail-race-bet/typescript/src/domain/BetApplication.spec.ts
@@ -3,7 +3,7 @@ import {BetRepositoryFake} from "./BetRepositoryFake";
 import {BetApplication} from "./BetApplication";
 import {Winners} from "./Winners";
 
-describe('BetApplication', () => {
+describe('BetApplication has', () => {
     let raceResultProvider: RaceResultProviderFake;
     let app: BetApplication;
 
@@ -12,11 +12,40 @@ describe('BetApplication', () => {
         app = new BetApplication(new BetRepositoryFake(), raceResultProvider);
 
     });
-    it('no winners when no Bet is placed', async () => {
+
+    it('no winner when no Bet is placed', async () => {
         expect(await app.getWinnersForLastRace()).toEqual(new Winners([]));
     });
 
-    it('win when the podium exactly matches the bet', () => {
+    describe("winners when the podium exactly matches the bet", () => {
+        it('exact match', () => {
+            // Place a bet through betApplication
+            // Configure race result provider simulator to have the corresponding podium
+            // Verify winners
+            fail('Write the test and implement it');
+        });
 
+        it('third place differs', () => {
+            // Place a bet through betApplication
+            // Configure race result provider simulator to have another podium
+            // Verify there is no winner
+            fail('Write the test and implement it');
+        });
+    })
+
+    it('no winner when bet is placed less that', () => {
+        // Place a bet through betApplication
+        // Configure race result provider simulator to have the corresponding podium but with a timestamp less than 3 seconds
+        // Verify there is no winner
+        fail('Write the test and implement it');
     });
+
+    it('no winner when the bet is older than the previous race', () => {
+        // Place a bet through betApplication
+        // Configure a race that is older than the bet
+        // Configure another race that is newer than the previous race and has a podium that match the pronostic
+        // Verify there is no winner
+        fail('Write the test and implement it');
+    });
+
 });
